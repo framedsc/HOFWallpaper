9f1181177d5b3b3d550d6ac5060652aa93c1e17f
originalnicodr <niconavall@gmail.com>
2023-12-03 15:12:13 -0300

    Added splash screen background image

--- src/components/splashScreen.js
+++ src/components/splashScreen.js
@@ -11,4 +11,4 @@ const splashScreenStyle = {
   animation: 'splashScreen .3s 1.5s cubic-bezier(.22,.61,.36,1) forwards',
 }
 
-export const splashScreen = <img src='https://cdn.discordapp.com/attachments/877962007524044810/927287125282529340/Wallpaper-1.png' style={splashScreenStyle} alt=''></img>
+export const splashScreen = <img src={require('../assets/Wallpaper-1.png')} style={splashScreenStyle} alt=''></img>
